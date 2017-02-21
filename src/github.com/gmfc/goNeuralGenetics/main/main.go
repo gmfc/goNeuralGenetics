dbbc0bf927b441dbd7eefbc1f8043059ac6d38ba
valsimot <valsimot@gmail.com>
2017-02-21 13:30:27 -0500

    async ok, XOR implementado

--- src/github.com/gmfc/goNeuralGenetics/main/main.go
+++ src/github.com/gmfc/goNeuralGenetics/main/main.go
@@ -2,43 +2,29 @@ package main
 
 import (
     "fmt"
-    "encoding/json"
     "neural"
+    "os"
 )
 
-
-func main() {
-/*
-  n := NewNode()
-  n.bias = 0.2
-  n.weights[0] = 0.5
-  fmt.Println(n)
-  
-  l := NewLayer()
-  l.nodes[0] = n
-  
-  nn := NewNetwork()
-  nn.layers[0] = l
-  fmt.Println(nn)
-*/
+func generateXORNet() neural.Network {
   
   nInA := neural.NewNode()
   nInB := neural.NewNode()
   
   nHA := neural.NewNode()
-  nHA.Weights["0"] = 0.1
-  nHA.Weights["1"] = 0.2
-  nHA.Bias = 0.5
+  nHA.Weights["0"] = 9.945131391752511
+  nHA.Weights["1"] = -7.065739412326366
+  nHA.Bias = -5.085902468767017
   
   nHB := neural.NewNode()
-  nHB.Weights["0"] = 0.11
-  nHB.Weights["1"] = 0.22
-  nHB.Bias = 0.5
+  nHB.Weights["0"] = 5.465975496452302
+  nHB.Weights["1"] = -3.934639404527843
+  nHB.Bias = 2.086773671209812
   
   nOA := neural.NewNode()
-  nOA.Weights["0"] = 0.111
-  nOA.Weights["1"] = 0.222
-  nOA.Bias = 0.5
+  nOA.Weights["0"] = 10.26414252538234
+  nOA.Weights["1"] = -9.913011252414435
+  nOA.Bias = 4.650809367187321
   
   
   layIn := neural.NewLayer()
@@ -56,20 +42,17 @@ func main() {
   NET.Layers["0"] = layIn
   NET.Layers["1"] = layH
   NET.Layers["2"] = layO
-
-
-  b, err := json.Marshal(NET)
-  if err != nil {
-    fmt.Println(err)
-    return
-  }
-  fmt.Println(string(b))
   
-  out := neural.RunNetwork(map[string]float64{ "0":0.4, "1":0.8 },NET)
-  fmt.Println(out)
+  return NET
+  
+  
+}
 
-    
 
-
+func main() {
+  
+  NET := generateXORNet()
+  out := neural.RunNetwork(map[string]float64{ "0":0, "1":1 },NET)
+  fmt.Println(out["0"])
   
 }
--- src/neural/network.go
+++ src/neural/network.go
@@ -39,26 +39,28 @@ func NewNetwork() Network {
 func RunNetwork(input map[string]float64, net Network) map[string]float64 {
   // m := map[string]string{ "key1":"val1", "key2":"val2" };
   var output map[string]float64
-  for k, lay := range net.Layers {
-    if k != "0" {
+  for layId, lay := range net.Layers {
+    if layId!= "0" {
       output =  make(map[string]float64)
-      
       for nodId, nod := range lay.Nodes {
         sum := nod.Bias
         
-        for from, weigh := range nod.Weights {
-          //fmt.Println(from)
-          //fmt.Println(weigh)
-          
-          sum += weigh * input[from]
-          //fmt.Println(sum)
-        }
-        output[nodId] = 1 / (1 + math.Exp(-sum))
+        // ASYNC STUFF!!
+        done := make(chan bool) // FLAG CHANEL
         
+        for from, weigh := range nod.Weights {
+          go func(ww float64, in float64) {
+            sum += ww * in
+            done <- true
+          }(weigh,input[from])
+        }
+        // Wait complete
+        for _ = range nod.Weights {
+          <-done
+        }
+        output[nodId] = 1 / (1 + math.Exp(-sum)) 
       }
       input = output
-      
-      
     }
   }
   return output
