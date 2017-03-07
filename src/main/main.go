30f747278a909d6796f9447a4770e50b64b14b5d
gabriel.mfcorreia <gabriel.mfcorreia@gmail.com>
2017-03-07 08:20:47 -0500

    fix

--- src/main/main.go
+++ src/main/main.go
@@ -1,79 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "neural"
-    //"os"
-    //"strconv"
-    "genetics"
+  "fmt"
+  "neuralArray"
 )
 
-func generateXORNet() neural.Network {
+
+func main() {  
+  setup := []int{2,2,1}
+  net := neuralArray.GenerateEmptyNet(setup)
+  net = neuralArray.Mutate(net,setup)
   
-  nInA := neural.NewNode()
-  nInB := neural.NewNode()
-  
-  nHA := neural.NewNode()
-  nHA.Weights["0"] = 9.945131391752511
-  nHA.Weights["1"] = -7.065739412326366
-  nHA.Bias = -5.085902468767017
-  
-  nHB := neural.NewNode()
-  nHB.Weights["0"] = 5.465975496452302
-  nHB.Weights["1"] = -3.934639404527843
-  nHB.Bias = 2.086773671209812
-  
-  nOA := neural.NewNode()
-  nOA.Weights["0"] = 10.26414252538234
-  nOA.Weights["1"] = -9.913011252414435
-  nOA.Bias = 4.650809367187321
-  
-  
-  layIn := neural.NewLayer()
-  layIn.Nodes["0"] = nInA
-  layIn.Nodes["1"] = nInB
-  
-  layH := neural.NewLayer()
-  layH.Nodes["0"] = nHA
-  layH.Nodes["1"] = nHB
-  
-  layO := neural.NewLayer()
-  layO.Nodes["0"] = nOA
-  
-  NET := neural.NewNetwork()
-  NET.Layers["0"] = layIn
-  NET.Layers["1"] = layH
-  NET.Layers["2"] = layO
-  
-  return NET
-  
-  
-}
+
+  fmt.Println("created net: " ,net)
 
 
-func main() {
-  NET := generateXORNet()
   
-  nNet := genetics.MutateNet(NET)
+  inp := []float64{1,1}
   
-  fmt.Println(NET)
-  fmt.Println(nNet)
   
-  outA := NET.RunNetwork(map[string]float64{ "0":1, "1":1 })
-  outB := nNet.RunNetwork(map[string]float64{ "0":1, "1":1 })
-  fmt.Println(outA["0"])
-  fmt.Println(outB["0"])
   
-  /*
-  a, err := strconv.ParseFloat(os.Args[1], 64)
-  b, err := strconv.ParseFloat(os.Args[2], 64)
-  if err != nil {
-    fmt.Println("Erro: ", err)
-  } else {
-    NET := generateXORNet()
-    out := NET.RunNetwork(map[string]float64{ "0":a, "1":b })
-    fmt.Println(out["0"])
-  }
-*/
-  
+  fmt.Println("out:", neuralArray.Run(net,inp))
 }
--- src/neuralArray/neuralArray.go
+++ src/neuralArray/neuralArray.go
@@ -11,7 +11,7 @@ func GenerateEmptyNet(setup []int) [][][]float64 {
   numberOfLayers := len(setup)
   N := make([][][]float64, numberOfLayers)// create neuralNet
   for l := 0; l < numberOfLayers; l++ {// fore layer...
-    neuronsOnLayer := setup[l]//see how many neurons it should have
+    neuronsOnLayer := setup[l]// see how many neurons it should have
     N[l] = make([][]float64, neuronsOnLayer)// create layer in network
     for neu := 0; neu < neuronsOnLayer; neu++ {// fore neuron on layer...
       // Size of neuron array is bias + weights, Ex.: L0 has 3 neurons, neuron on L1 will have setup[0](3) + 1 (bias) elements in array (total 4)
@@ -33,7 +33,7 @@ func GenerateEmptyNet(setup []int) [][][]float64 {
 func Mutate(net [][][]float64,setup []int) [][][]float64 {
   NewNet := GenerateEmptyNet(setup)
   randSource := rand.NewSource(time.Now().UnixNano())
-  //deterministicSource := rand.NewSource(42)
+  //randSource := rand.NewSource(42)
   randomizer := rand.New(randSource)
   for l:=0;l<len(net);l++{// fore layer...
     for n:=0;n<len(net[l]);n++{// fore neuron...
